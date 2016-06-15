4ba6968642110baf9cf9949323f15b6b370cb0aa
Andris Shvarcs <andris.svarcs@yahoo.com>
2016-06-15 18:28:57 +0100

    Grunt tasks for getting http requests

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,19 @@ module.exports = function (grunt) {
      * You must have node.js on your machine: https://nodejs.org/en/download/
      * CD to /app/source/
      * Terminal: npm install
-     * grunt dev
+     * grunt dev --target=dev
+     * grunt dist --target=dist
      *
      * */
 
-    var path = grunt.option('target') || 'dev';
+    //var path = grunt.option('target') || 'dev';
+    var path = grunt.cli.tasks[0]; //getting global task, not --target
+    console.log('LOG: Grunt current task: ' + grunt.cli.tasks[0]);
+    var pathToLocalWeb = 'http://generator.local/';
 
+    grunt.config.merge({
+        http: {}
+    });
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -32,24 +39,16 @@ module.exports = function (grunt) {
         },
         clean: {
             options: {
-                force:true
+                force: true
             },
-            web: {
+            dev: {
                 src: ['../app/webroot/css/web/']
             }
         },
         watch: {
             scss: {
-                files: ['scss/web/**/*.scss'],
-                tasks: ['sass:web']
-            },
-            cssAdmin: {
-                files: ['css/admin/*.css'],
-                tasks: ['cssmin:webCSS']
-            },
-            scssAdmin: {
-                files: ['scss/admin/**/*.scss'],
-                tasks: ['sass:admin']
+                files: ['source/scss/**/*.scss'],
+                tasks: ['sass:' + path]
             }
         }
     });
@@ -57,14 +56,63 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-http');
+
+    grunt.task.registerTask('createPages', 'description', function (){
+        console.log('yo');
+
+        var src = ['source/files/**/*.php', '!source/files/inc/**'];
+        
+        var pages = grunt.file.expand({cwd: ''}, src);
+
+        grunt.log.debug(pages);
+        console.log('Pages: ', pages);
+
+        for (var i = 0; i < pages.length; i++) {
+            var http = {};
+            var arrSegments = pages[i].split('/');
+            var pageName = arrSegments[arrSegments.length - 1];
+
+            console.log('url: ', pages[i]);
+            console.log('dest: ', 'dev/' + pageName);
+            var config = {
+                http: {
+                    dev: {
+                        options: {
+                            url: pathToLocalWeb + pages[i]
+                        },
+                        dest: 'dev/' + arrSegments[arrSegments.length -1]
+                    }
+                }
+            };
+            grunt.config.merge(config);
+        }
+        
+        /*var config = {
+            http: {
+                dev: {
+                    options: {
+                        url: pathToLocalWeb + 'test.php'
+                    },
+                    dest: 'dev/test.html'
+                }
+            }
+        };
+        grunt.config.merge(config);*/
+        
+    });
+
+
+
+    grunt.registerTask('test', [
+        'createPages',
+        'http'
+    ]);
 
     grunt.registerTask('dev', [
         /*'clean:env',*/
-        'sass:web',
-        'sass:admin',
-        'cssmin:adminCSS',
+        'sass:' + path,
         'watch'
     ]);
 };
-
 
