7362fa18bb0dcd036cbaa085ab9e0c9d600fbec3
Andris Shvarcs <andris.svarcs@yahoo.com>
2016-12-06 21:47:56 +0000

    Long pages scroll to top after entering

--- source/js/app/entryPoint.js
+++ source/js/app/entryPoint.js
@@ -4,6 +4,7 @@ define(['app/components/prism'], function (prism) {
         // This is called always from Barba.js after page is loaded
         /*var h1 = document.getElementsByTagName('h1');
         h1[0].style.color = 'green';*/
+        document.body.scrollTop = 0;
         prism();
     }
 });
